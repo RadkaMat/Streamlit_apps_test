b6bdba11067d58618e18490c79f00eec7e7f4862
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-01-03 11:34:48 +0100

    Update první strana.py
    oprava linux příkazu přidání výstupu 2

--- to_do_list_with_streamlit/pages/první strana.py
+++ to_do_list_with_streamlit/pages/první strana.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 
-st.text(os.system('touch vystup_prikazu.txt'))
-st.text(os.system('pwd > vystup_prikazu.txt'))
-st.text(os.system('ls >> vystup_prikazu.txt'))
+os.system('touch vystup_prikazu.txt')
+os.system('pwd > vystup_prikazu.txt')
+os.system('ls >> vystup_prikazu.txt')
 st.write('První strana')
