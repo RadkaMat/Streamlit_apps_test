bd5a37912622837291daf9ff87509c11098c5615
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-01-03 11:40:01 +0100

    Update první strana.py
    linuxová kontrola

--- to_do_list_with_streamlit/pages/první strana.py
+++ to_do_list_with_streamlit/pages/první strana.py
@@ -5,7 +5,7 @@ os.system('touch vystup_prikazu.txt')
 os.system('pwd > vystup_prikazu.txt')
 os.system('ls >> vystup_prikazu.txt')
 
-with open(cestax, mode='r', encoding='UTF-8') as souborx:
+with open('vystup_prikazu.txt', mode='r', encoding='UTF-8') as souborx:
         seznam_ukolux = souborx.readlines()
 st.write(seznam_ukolux)
 
