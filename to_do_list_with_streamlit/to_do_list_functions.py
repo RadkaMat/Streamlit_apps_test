5354c2e7c33719cf94a35618428a7da17fc2fe9f
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-01-03 13:59:04 +0100

    Create to_do_list_functions.py
    překlad do angličtiny

---
+++ to_do_list_with_streamlit/to_do_list_functions.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from datetime import datetime
+
+PATH = r'to_do_list_with_streamlit/to_do_list_result.txt'
+
+
+def get_to_do_list(pathx=PATH):
+    """
+        Function loads the to-do list from streamlit linux cloud server.
+        Returns to-do list.
+    """
+    with open(pathx, mode='r', encoding='UTF-8') as filex:
+        to_do_listx = filex.readlines()
+    return to_do_listx
+
+
+def save_to_do_list(to_do_listx, pathx=PATH):
+    """ Function saves the to-do list to streamlit linux cloud server,,. """
+    with open(pathx, mode='w', encoding='UTF-8') as filex:
+        filex.writelines(to_do_listx)
