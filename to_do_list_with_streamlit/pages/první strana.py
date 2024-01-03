c6d0bbd408569ab271b556a7dcff4fddbd7541de
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-01-03 11:33:44 +0100

    Update první strana.py
    linuxová kontrola vytvoření souboru s výstupem

--- to_do_list_with_streamlit/pages/první strana.py
+++ to_do_list_with_streamlit/pages/první strana.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 
-st.text(os.system('pwd'))
-st.text(os.system('ls'))
+st.text(os.system('touch vystup_prikazu.txt'))
+st.text(os.system('pwd > vystup_prikazu.txt'))
+st.text(os.system('ls >> vystup_prikazu.txt'))
 st.write('První strana')
