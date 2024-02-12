59c66c7336fb3902fbdc6051c2c0c199919c8d53
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-02-12 18:52:35 +0100

    Update první strana.py
    změna uspořádání

--- to_do_list_with_streamlit/pages/první strana.py
+++ to_do_list_with_streamlit/pages/první strana.py
@@ -6,9 +6,9 @@ os.system('pwd > vystup_prikazu.txt')
 os.system('ls >> vystup_prikazu.txt')
 os.system('ls /mount/src/application_python/to_do_list_with_streamlit >> vystup_prikazu.txt')
 
+st.write('Linux commands: touch, pwd and ls')
+st.write('První strana')
+
 with open('vystup_prikazu.txt', mode='r', encoding='UTF-8') as souborx:
         seznam_ukolux = souborx.readlines()
 st.write(seznam_ukolux)
-
-st.write('Linux commands: touch, pwd and ls')
-st.write('První strana')
