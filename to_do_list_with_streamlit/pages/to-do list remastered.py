e3d01ceb3272ec5bc0d4ca4cbbf74f79d062997f
RadkaMat <104318039+RadkaMat@users.noreply.github.com>
2024-12-29 17:00:47 +0100

    Remaster main - error handling - set background color.

--- to_do_list_with_streamlit/pages/to-do list remastered.py
+++ to_do_list_with_streamlit/pages/to-do list remastered.py
@@ -17,7 +17,7 @@ PAGE_BG_STYLE = """
 background-image: url("https://images.unsplash.com/photo-1722082839802-18b18cb23a62");
 background-size: cover;
 
-[data-testid="stMainBlockContainer"] {
+.stForm {
     background-color: #262626;
 }
 </style>
